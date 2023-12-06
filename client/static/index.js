f5e46e1b27945f938eb9fdbf768fa73ec223f176
Pondigo <lenora.pondigo@gmail.com>
2023-12-05 21:38:16 -0600

    Update static site

--- client/static/index.js
+++ client/static/index.js
@@ -1,4 +1,3 @@
-"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,10 +7,39 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+import contactsTable from "./table/index.js";
+import modalForm from "./modalForm/index.js";
+import * as api from "./api/index.js";
+const table = new contactsTable();
+const modal = new modalForm();
+function addCreateButton() {
+    var addButton = document.createElement("div");
+    addButton.id = "add-button";
+    var button = document.createElement("button");
+    button.className = "Btn";
+    button.innerHTML = `<div class="sign">+</div>
+  <div class="text">Nuevo</div>`;
+    button.addEventListener("click", () => __awaiter(this, void 0, void 0, function* () {
+        modal.render("create");
+    }));
+    addButton.appendChild(button);
+    document.body.appendChild(addButton);
+}
 function getContacts() {
     return __awaiter(this, void 0, void 0, function* () {
-        const hello = "hello world";
-        console.log(hello);
+        try {
+            const result = yield api.getContacts();
+            const titleHeader = document.getElementById("titleHeader");
+            if (titleHeader) {
+                titleHeader.className = "title";
+                console.log("Change clase");
+            }
+            table.renderContacts(result);
+            addCreateButton();
+        }
+        catch (err) {
+            console.error(err);
+        }
     });
 }
 getContacts();
